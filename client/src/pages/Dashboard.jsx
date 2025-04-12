9f56a98eb2a72ee105a2b6c8b0fc1b543dc2235b
sneha0715 <157895540+sneha0715@users.noreply.github.com>
2025-04-12 23:44:43 +0530

    project initial set up with login and register using deployed backend for testing

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../assets/styles/dashboard.css"
+import Header from "../components/Header"
+
+const Dashboard = () => {
+  return (
+    <>
+    <Header />
+    <div className='dashboard-wrapper'>
+      
+     <h1>Sneha pgll hai </h1>            
+            
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import "../assets/styles/Login.css"
+import { RiLockPasswordLine } from "react-icons/ri";
+import { MdOutlineEmail } from "react-icons/md";
+import axios from "axios";
+import Header from "../components/Header";
+import {useNavigate} from "react-router-dom";
+
+const initialFormData = {
+  email: '',
+  password: ''
+}
+const login = () => {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const navigate=useNavigate()
+
+
+  const handleChange = (event) => {
+
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+
+    try{
+      const response=await axios.post("https://auth-backend-08hn.onrender.com/login",formData)
+      console.log(response)
+    }catch(error){
+      console.log(error)
+    }
+    
+    setFormData(initialFormData)
+  }
+
+  return (
+  <> 
+       <Header />
+    <div className='container'>
+      <div className="wrapper">
+        <form action=""
+          onSubmit={(event) => { handleSubmit(event) }}
+        >
+
+          <h1 className='login'>Login</h1>
+          <div className='input-wrapper'>
+            <MdOutlineEmail className='icon' />
+            <input
+              type="email"
+              placeholder='Email'
+              name='email'
+              value={formData.email}
+              onChange={(event) => { handleChange(event) }}
+              className='input-field' />
+          </div>
+
+          <div className='input-wrapper'>
+            <RiLockPasswordLine className='icon' />
+            <input
+              type="password"
+              placeholder='Password'
+              name='password'
+              value={formData.password}
+              onChange={(event) => { handleChange(event) }}
+              className='input-field' />
+          </div>
+          <button className='btn-login'>Login</button>
+        </form>
+
+        <span className='text-footer'>New Here ?
+          <span className='text-signUp ' onClick={()=>{navigate('/register')}}> Sign Up</span>
+        </span>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default login
